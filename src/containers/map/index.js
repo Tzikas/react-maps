8bf8fc2014bacbc0ea58042c96d643bbf2f19393
squirrl <niko.tzikas@gmail.com>
2017-08-10 06:56:32 -0400

    started moving stuff into the reducers

--- src/containers/map/index.js
+++ src/containers/map/index.js
@@ -3,8 +3,9 @@ import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import {
-  setLocation,
-  addMap
+	setLocation,
+	addMap,
+	getFirstMap
 } from './reducers'
 
 import RandomCityMap from '../random-cities'
@@ -14,40 +15,32 @@ import raf from "raf";
 
 import {
 	default as React,
-	Component,
+		Component,
 } from "react";
+
 
 import {
 	withGoogleMap,
-	GoogleMap,
-	Marker,
+		GoogleMap,
+		Marker,
 } from "react-google-maps";
 
+
 const geolocation = (
-	canUseDOM && navigator.geolocation ?
-	navigator.geolocation : 
-	({
-		getCurrentPosition(success, failure) {
-			failure(`Your browser doesn't support geolocation.`);
-		},
-	})
-);
+		canUseDOM && navigator.geolocation ?
+		navigator.geolocation : 
+		({
+			getCurrentPosition(success, failure) {
+				failure(`Your browser doesn't support geolocation.`);
+			},
+		})
+		);
 
 const GeolocationExampleGoogleMap = withGoogleMap(props => (
-	<GoogleMap
-		defaultZoom={12}
-		center={props.center}
-	>
-
-		<Marker
-			position={props.center}
-			title='You are (near) here.'
-		/>
-
-
-
-	</GoogleMap>
-));
+			<GoogleMap defaultZoom={12} center={props.center} >
+				<Marker position={props.center} title='You are (near) here.' />
+			</GoogleMap>
+			));
 
 
 const outside = ()=>{
@@ -65,53 +58,46 @@ class YourMap extends Component {
 			center: null,
 		};
 		this.state = state;
-		this.inside = this.inside.bind(this)
 	}
 
 	isUnmounted = false;
 
 	componentDidMount() {
 
-		geolocation.getCurrentPosition((position) => {
-			if (this.isUnmounted) {
-				return;
-			}
-			this.setState({
-				center: {
-					lat: position.coords.latitude,
-					lng: position.coords.longitude,
-				}
-			});
-			console.log(this)
-			let winner = Math.floor(Math.random()*4)
-			this.setState({winner:winner})
+		this.props.getFirstMap();
+		console.log(this);
+		//put this inside an action and then add a promise 
+		/*geolocation.getCurrentPosition((position) => {
+		  if (this.isUnmounted) {
+		  return;
+		  }
+		  this.setState({
+		  center: {
+		  lat: position.coords.latitude,
+		  lng: position.coords.longitude,
+		  }
+		  });
+		  let winner = Math.floor(Math.random()*4)
+		  this.setState({winner:winner})
+
+
+
+		//Calculate teh distance here... 
+		//
+		//
 
 		}, (reason) => {
-			if (this.isUnmounted) {
-				return;
-			}
-			this.setState({
-				center: {
-					lat: 60,
-					lng: 105,
-				}
-			});
-		});
-
-		//this.inside();
-	}
-	inside(){
-		console.log('inside', this)
-		//this.forceUpdate()
-	 	//this.setState(this.state)	
-		//this.setState({ key: Math.random() });
+		if (this.isUnmounted) {
+		return;
+		}
 		this.setState({
-			center: {
-				lat: 60,
-				lng: 105,
-			}
+		center: {
+		lat: 60,
+		lng: 105,
+		}
 		});
-
+		});*/
+		//this.inside();
 	}
 
 	componentWillUnmount() {
@@ -126,40 +112,42 @@ class YourMap extends Component {
 			}else{
 				var winner = false; 
 			}
-    			rands.push(<RandomCityMap key={i} winner={winner} inside={this.inside}/>);
+			rands.push(<RandomCityMap key={i} winner={winner} inside={this.inside}/>);
 		}
 		return (
 
-			<div>
-			<button onClick={this.props.addMap}>Push</button>
-			<GeolocationExampleGoogleMap
-				containerElement={
-					<div style={{ height: `200px` }} />
-				}
-				mapElement={
-					<div style={{ height: `200px` }} />
-				}
-				center={this.state.center}
-			/>
-			{rands}		
-				
-			</div>
-		);
+				<div>
+				<button onClick={this.props.addMap}>Push</button>
+				<GeolocationExampleGoogleMap
+					containerElement={
+						<div style={{ height: `200px` }} />
+					}
+					mapElement={
+						<div style={{ height: `200px` }} />
+					}
+					center={this.state.center}
+				/>
+				{rands}		
+
+				</div>
+		       );
 	}
 }
 
 const mapStateToProps = state => ({
-  count: state.counter.count,
-  randomLocations:state.randomMap.randomLocations
+	count: state.counter.count,
+      	randomLocations:state.randomMap.randomLocations,
+      	position: state.yourMap
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  setLocation,
-  addMap,
-  changePage: () => push('/about-us')
+	setLocation,
+      	addMap, 
+      	getFirstMap,
+      	changePage: () => push('/about-us')
 }, dispatch)
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(YourMap)
+			mapStateToProps,
+			mapDispatchToProps
+		)(YourMap)
--- src/containers/map/reducers.js
+++ src/containers/map/reducers.js
@@ -1,53 +1,150 @@
+import canUseDOM from "can-use-dom";
+import {
+	withGoogleMap,
+		GoogleMap,
+		Marker,
+} from "react-google-maps";
+
+
+
 export const LOCATION = 'adfdsf/LOCATasdION/usethisforoutsidedispatchcalls' 
 export const ADDMAP = ''; 
+export const SETFIRSTMAP = ''; 
 
 
 const initialState = {
-  theLocation: false, 
-  maps: [] 	
+	theLocation: false, 
+	maps: [] 	
 }
 
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case LOCATION: 
-      state.theLocation = action.payload
-      return {
-      	...state,
-	theLocation: state.theLocation
+	switch (action.type) {
+		case LOCATION: 
+			state.theLocation = action.payload
+			return {
+				...state,
+				theLocation: state.theLocation
 
-      }
-    case ADDMAP: 
-      return {
-      	...state,
-	maps: [...state.maps, action.payload]
-      }
+			}
+		case ADDMAP: 
+			return {
+				...state,
+				maps: [...state.maps, action.payload]
+			}
+		case SETFIRSTMAP:
+			let r = Math.random(); 
+			console.log(action)
+			return {
+				...state,
+				r: r				
+			}
 
 
-        
-    default:
-      return state
-  }
+		default:
+			return state
+	}
 }
 
 
 export const setLocation = (yo) => {
-  return dispatch => {
-    dispatch({
-      type: LOCATION,
-      payload:yo
-    })
-  }
+	return dispatch => {
+		dispatch({
+			type: LOCATION,
+		payload:yo
+		})
+	}
 }
 
 
 export const addMap = (yo) => {
-  return dispatch => {
-    dispatch({
-      type: ADDMAP,
-      payload:yo
-    })
-  }
+	return dispatch => {
+		dispatch({
+			type: ADDMAP,
+		payload:yo
+		})
+	}
 }
+
+
+export const setFirstMap = (position) => {
+	console.log(position)
+		return{
+			type:"SETFIRSTMAP",
+			payload:position	
+		}
+}
+
+
+
+
+
+
+
+
+
+const geolocation = (
+		canUseDOM && navigator.geolocation ?
+		navigator.geolocation : 
+		({
+			getCurrentPosition(success, failure) {
+				failure(`Your browser doesn't support geolocation.`);
+			},
+		})
+		);
+
+export const getFirstMap = () => {
+	console.log('wtf')
+		return dispatch =>{
+
+
+			geolocation.getCurrentPosition((position) => {
+				if (this.isUnmounted) {
+					return;
+				}
+				console.log(position)
+				/*	this.setState({
+					center: {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+					}
+					});*/
+
+				//
+				dispatch(setFirstMap(position))
+
+				//CALL ANOTHER AJAX FETCH HERE
+				//DISPATCH THAT RETURNING DATA INTO CONST SETSECOND MAP
+
+
+
+
+
+				//console.log(this)
+				//let winner = Math.floor(Math.random()*4)
+				//this.setState({winner:winner})
+
+
+
+				//Calculate teh distance here... 
+				//
+				//
+
+			}, (reason) => {
+				if (this.isUnmounted) {
+					return;
+				}
+				this.setState({
+					center: {
+						lat: 60,
+						lng: 105,
+						}
+				});
+			});
+
+
+
+		} 	
+}	
 
 
 
